5b6554a674056c86c014291cb314b2f957ee2197
Carlos Fernandes | Machine Learning <69088161+carlosfernandescrypt@users.noreply.github.com>
2023-08-30 10:07:16 -0300

    Add files via upload

---
+++ Atividade4/q4.c
@@ -0,0 +1,30 @@
+/* Dados três valores A, B e C, construa um algoritmo, que imprima os valores de forma descendente (do maior para o menor). */
+
+#include <stdio.h>
+
+int main() {
+    int a, b, c, aux;
+    printf("Digite o primeiro número:\n");
+    scanf("%d", &a);
+    printf("Digite o segundo número:\n");
+    scanf("%d", &b);
+    printf("Digite o terceiro número:\n");
+    scanf("%d", &c);
+    if (a < c) {
+        aux = c;
+        c = a;
+        a = aux;
+    }
+    if (a < b) {
+        aux = b;
+        b = a;
+        a = aux;
+    }
+    if (b < c) {
+        aux = c; 
+        c = b;
+        b = aux;
+    }
+    printf("Números em ordem descendente: ");
+    printf("%d %d %d", a, b, c);
+}
