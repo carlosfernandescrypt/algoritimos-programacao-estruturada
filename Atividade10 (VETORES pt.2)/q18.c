f86ec00356741834164f52781d6d18b94f3bdcd4
Carlos Fernandes | Machine Learning <69088161+carlosfernandescrypt@users.noreply.github.com>
2023-11-01 09:57:47 -0300

    Add files via upload

---
+++ Atividade10 (VETORES pt.2)/q18.c
@@ -0,0 +1,30 @@
+/* 18. faça um programa que leia um vetor de 10 numeros. leia um numero x. conte os multiplos de um numero inteiro x num vetor e mostre-os na tela 
+*/
+#include<stdio.h>
+
+int main() {
+    int vetor[10];
+    int x, i, count;
+
+    printf("Digite os 10 numeros do vetor:\n");
+
+    for (i = 0; i < 10; i++) {
+        scanf("%d", &vetor[i]);
+    }
+
+    printf("Digite um numero x:\n");
+    scanf("%d", &x);
+
+    count = 0;
+
+    for (i = 0; i < 10; i++) {
+        if (vetor[i] % x == 0) {
+            count++;
+            printf("%d ", vetor[i]);
+        }
+    }
+
+    printf("\nTotal de multiplos de %d: %d\n", x, count);
+
+    return 0;
+}
