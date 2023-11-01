218a7d808059b6b3288c432226049c1dffe31417
Carlos Fernandes | Machine Learning <69088161+carlosfernandescrypt@users.noreply.github.com>
2023-11-01 10:14:42 -0300

    Add files via upload

---
+++ Atividade10 (VETORES pt.2)/q22.c
@@ -0,0 +1,31 @@
+/* 22. faça um programa que leia dois vetores de 10 posições e calcule outro vetor contendo, nas posições pares os valores do primeiro e nas posições impares os valores do segundo.
+*/
+
+#include <stdio.h>
+
+int main() {
+    int vetor1[10], vetor2[10], vetor3[10 * 2];
+    int i, j = 0;
+
+    printf("Digite os 10 numeros inteiros do primeiro vetor:\n");
+    for (i = 0; i < 10; i++) {
+        scanf("%d", &vetor1[i]);
+    }
+
+    printf("Digite os 10 numeros inteiros do segundo vetor:\n");
+    for (i = 0; i < 10; i++) {
+        scanf("%d", &vetor2[i]);
+    }
+
+    for (i = 0; i < 10; i++) {
+        vetor3[j++] = vetor1[i];
+        vetor3[j++] = vetor2[i];
+    }
+
+    printf("Vetor resultante:\n");
+    for (i = 0; i < 10 * 2; i++) {
+        printf("%d ", vetor3[i]);
+    }
+
+    return 0;
+}
