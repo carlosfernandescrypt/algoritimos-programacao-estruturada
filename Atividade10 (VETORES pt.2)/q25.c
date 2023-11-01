46ae1e3a3e00c210f650c9f440541f7cd08bbfa6
Carlos Fernandes | Machine Learning <69088161+carlosfernandescrypt@users.noreply.github.com>
2023-11-01 10:22:47 -0300

    Add files via upload

---
+++ Atividade10 (VETORES pt.2)/q25.c
@@ -0,0 +1,24 @@
+/* 25. faça um programa que preencha um vetor de tamanho 100 com os 100 primeiros naturais que não são múltiplos de 7 ou que terminam com 7 */
+
+#include <stdio.h>
+
+int main() {
+    int vetor[100];
+    int count = 0;
+    int number = 0;
+
+    while (count < 100) {
+        if (number % 7 != 0 && number % 10 != 7) {
+            vetor[count] = number;
+            count++;
+        }
+        number++;
+    }
+
+    printf("Vetor:\n");
+    for (int i = 0; i < 100; i++) {
+        printf("%d ", vetor[i]);
+    }
+
+    return 0;
+}
